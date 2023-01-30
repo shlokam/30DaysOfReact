b155bec4ffcf9e7750e34527991a4056d5191410
shlokam <shloka.mahesheka012@gmail.com>
2023-01-30 18:45:39 +0530

    Added Day3 Newsletter .

--- Exercises/Day3/day3/src/FrontEnd/FrontEnd.js
+++ Exercises/Day3/day3/src/FrontEnd/FrontEnd.js
@@ -7,22 +7,24 @@ import './FrontEnd.css';
 
 function FrontEnd(){
     return(
-        <div 
+        <div className="frontEnd"
         //style={{width:'830px',height:'120px',
         //overflowX:'scroll',overflowY:'hidden'
         //}}
         >
             <h1>FrontEnd Technologies</h1>
             <img src={html} 
-            style={{float:'left',width:'260',height:'190'}}
+            // style={{float:'left',width:'10',height:'10'}}
             />
             <img src={css} 
-            style={{float:'left',width:'160',height:'90'}}
+            // style={{float:'left',width:'10',height:'10'}}
             />
-             <img src={js} 
+             <img src={js}
+            //  style={{float:'left',width:'10',height:'10'}} 
             //style={{width:'10rem' ,height:'10rem' }}
             />
             <img src={react} 
+            // style={{float:'left',width:'10',height:'10'}}
             //style={{width:'10rem' ,height:'10rem' }} */}
             />
 
